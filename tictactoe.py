e18aea3e09082ed390d9510389961c716fd26ee4
Trevor Hiebert <hieballa@gmail.com>
2011-12-28 20:53:41 -0600

    start on game

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,27 @@
-import random
+print ("Hello World!")                             #Joke
+print ("Welcome to my Tic Tac Toe game!")          #Welcome screen
+print (" ")                                        #
+victory = none                                     #Victory status
+while victory == none:                             #Starting the while loop
+	{                                              #
+	print ("Board key:")                           #Board Key. Will appear after every move
+	print ("1 2 3")                                # 
+	print ("4 5 6")                                #
+	print ("7 8 9")                                #
+	var1 = "~"                                     #Initial variable assignment
+	var2 = "~"                                     #
+	var3 = "~"                                     #
+	var4 = "~"                                     #
+	var5 = "~"                                     #
+	var6 = "~"                                     #
+	var7 = "~"                                     #
+	var8 = "~"                                     #
+	var9 = "~"                                     #
+	print ("")                                     #Breaks
+	print ("")                                     #
+	print ("")                                     #
+	print (var1, var2, var3)                       #First iteration of the empty board
+	print (var4, var5, var6)                       #
+	print (var7, var8, var9)                       #
+	print ("")                                     #Break
+	var = input("Please enter your selection:")    #Placement selection
