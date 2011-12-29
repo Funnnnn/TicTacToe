0ad2cb5ba6609db0bba320fd660b1d604ae52bb7
Trevor Hiebert <hieballa@gmail.com>
2011-12-28 22:07:47 -0600

    deal with input

--- tictactoe.py
+++ tictactoe.py
@@ -13,6 +13,56 @@ while victory == 'none': # Starting the while loop
 	print(var4, var5, var6)
 	print(var7, var8, var9)
 	var = input('\nPlease enter your selection :') # Placement selection
-	if var == '3':
-		print('\n\n\n\n\n...yo mama')
-		victory = 'false'
+	if var == '1':
+		if var1 == '~'
+			var1 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '2':
+		if var2 == '~'
+			var2 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '3':
+		if var3 == '~'
+			var3 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '4':
+		if var4 == '~'
+			var4 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '5':
+		if var5 == '~'
+			var5 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '6':
+		if var6 == '~'
+			var6 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '7':
+		if var7 == '~'
+			var7 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '8':
+		if var8 == '~'
+			var8 = 'X'
+		else
+			print('Invalid selection')
+	elif var == '9':
+		if var9 == '~'
+			var9 = 'X'
+		else
+			print('Invalid selection')
+	elif var == 'Trevor is amazing':
+		victory = 'true'
+	else
+		print('Invalid selection')
+if victory == 'true'
+	print('Congratulations! You win!')
+else
+	print('You lost. So hard.')
