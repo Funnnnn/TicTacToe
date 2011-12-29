b5538500484ee2d9d0421a3179b0c08407028374
Trevor Hiebert <hieballa@gmail.com>
2011-12-28 22:31:26 -0600

    add print to display final move

--- tictactoe.py
+++ tictactoe.py
@@ -1,18 +1,18 @@
-print('Hello World!') # Joke
-print('Welcome to my Tic Tac Toe game!\n') # Welcome screen
-victory = 'none' # Victory status
+print('Hello World!')
+print('Welcome to my Tic Tac Toe game!\n')
+victory = 'none'
 var1, var2, var3 = '~', '~', '~'
 var4, var5, var6 = '~', '~', '~'
 var7, var8, var9 = '~', '~', '~'
-while victory == 'none': # Starting the while loop
-	print('Board key:') # Board Key. Will appear after every move
+while victory == 'none':
+	print('Board key:')
 	print('1 2 3') 
 	print('4 5 6')
 	print('7 8 9\n \n \n')
-	print(var1, var2, var3) # First iteration of the empty board
+	print(var1, var2, var3)
 	print(var4, var5, var6)
 	print(var7, var8, var9)
-	var = input('\nPlease enter your selection :') # Placement selection
+	var = input('\nPlease enter your selection :')
 	if var == '1':
 		if var1 == '~':
 			var1 = 'X'
@@ -62,15 +62,18 @@ while victory == 'none': # Starting the while loop
 		victory = 'true'
 	else:
 		print('\nInvalid selection\n\n\n')
-	if (var1 == 'X' and var2 == 'X' and var3 == 'X') or
-	(var4 == 'X' and var5 == 'X' and var6 == 'X') or
-	(var7 == 'X' and var8 == 'X' and var9 == 'X') or
-	(var1 == 'X' and var4 == 'X' and var7 == 'X') or
-	(var2 == 'X' and var5 == 'X' and var8 == 'X') or
-	(var3 == 'X' and var6 == 'X' and var9 == 'X') or
-	(var1 == 'X' and var5 == 'X' and var9 == 'X') or
-	(var3 == 'X' and var5 == 'X' and var7 == 'X')
+	if (var1 == 'X' and var2 == 'X' and var3 == 'X' or
+	var4 == 'X' and var5 == 'X' and var6 == 'X' or
+	var7 == 'X' and var8 == 'X' and var9 == 'X' or
+	var1 == 'X' and var4 == 'X' and var7 == 'X' or
+	var2 == 'X' and var5 == 'X' and var8 == 'X' or
+	var3 == 'X' and var6 == 'X' and var9 == 'X' or
+	var1 == 'X' and var5 == 'X' and var9 == 'X' or
+	var3 == 'X' and var5 == 'X' and var7 == 'X'):
 		victory = 'true'
+print(var1, var2, var3)
+print(var4, var5, var6)
+print(var7, var8, var9)
 if victory == 'true':
 	print('\nCongratulations! You win!')
 else:
