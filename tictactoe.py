319357de05cc82f8c5bd7ba3bc858d36f5fcc0b5
Trevor Hiebert <hieballa@gmail.com>
2011-12-29 22:43:21 -0600

    clean up assignment of var#

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,7 @@
 print('\n\n\nHello World!')
 print('Welcome to my Tic Tac Toe game!\n')
 victory = 'none'
-var1, var2, var3 = '~', '~', '~'
-var4, var5, var6 = '~', '~', '~'
-var7, var8, var9 = '~', '~', '~'
+var1, var2, var3, var4, var5, var6, var7, var8, var9 = '~' * 9
 while victory == 'none':
 	valid = 'true'
 	print('Board key:')
